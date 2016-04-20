c11747444b4576cfdd36eb85b43c3a6ae4cda4ea
Adam Bergman <adam@fransvilhelm.com>
2016-04-20 10:40:18 +0200

    Setting up tests

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export default function Header(props) {
-  return <h1>{props.title}</h1>;
+// export default function Header(props) {
+//   return (<h1>{props.title}</h1>);
+// }
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <h1>{this.props.title}</h1>
+    );
+  }
 }
